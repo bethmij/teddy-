f1a47826b4a7f796cb9762802b6ec5ad71bd2e15
Bethmi Jayamila <bethmij@gmail.com>
2025-03-19 00:20:37 +0530

    frontend -  update home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import Banner from '../components/Banner'
+import FeaturedProduct from '../components/FeaturedProduct'
 import HeaderSlider from '../components/HeaderSlider'
 
 export default function Home() {
-  return (
-    <div>
-      <HeaderSlider />
-    </div>
-  )
+    return (
+        <div>
+            <HeaderSlider />
+            <FeaturedProduct/>
+            <Banner />
+        </div>
+    )
 }
