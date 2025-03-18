793e43114939d3c8bd20167bd42d5245e02aa4b0
Bethmi Jayamila <bethmij@gmail.com>
2025-03-19 00:17:45 +0530

    frontend -  add header slider

---
+++ frontend/src/components/HeaderSlider.tsx
@@ -0,0 +1,99 @@
+import { useState, useEffect } from "react";
+import { assets } from "../assets/assets";
+
+const HeaderSlider = () => {
+  const sliderData = [
+    {
+      id: 1,
+      title: "Meet the Softest Companion - Your Perfect Teddy Bear Awaits!",
+      offer: "Limited Time Offer 30% Off",
+      buttonText1: "Buy now",
+      buttonText2: "Find more",
+      imgSrc: assets.teddy_1,
+    },
+    {
+      id: 2,
+      title: "Cuddle with Comfort - Discover the Huggable Teddy Bears Today!",
+      offer: "Hurry up only few lefts!",
+      buttonText1: "Shop Now",
+      buttonText2: "Explore Deals",
+      imgSrc: assets.teddy_2,
+    },
+    {
+      id: 3,
+      title: "The Ultimate Teddy Bear Experience - Soft, Cuddly, and Always There for You!",
+      offer: "Exclusive Deal 40% Off",
+      buttonText1: "Order Now",
+      buttonText2: "Learn More",
+      imgSrc: assets.teddy_3,
+    },
+  ];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % sliderData.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [sliderData.length]);
+
+  const handleSlideChange = (index:any) => {
+    setCurrentSlide(index);
+  };
+
+  return (
+    <div className="overflow-hidden relative w-full">
+      <div
+        className="flex transition-transform duration-700 ease-in-out"
+        style={{
+          transform: `translateX(-${currentSlide * 100}%)`,
+        }}
+      >
+        {sliderData.map((slide, index) => (
+          <div
+            key={slide.id}
+            className="flex flex-col-reverse md:flex-row items-center justify-between bg-[#F8F2EE] py-8 md:px-14 px-5 mt-6 rounded-xl min-w-full"
+          >
+            <div className="md:pl-8 mt-10 md:mt-0 w-1/2">
+              <p className="md:text-base text-orange-600 pb-1">{slide.offer}</p>
+              <h1 className="max-w-lg md:text-[40px] md:leading-[48px] text-2xl font-semibold">
+                {slide.title}
+              </h1>
+              <div className="flex items-center mt-4 md:mt-6 ">
+                <button className="md:px-10 px-7 md:py-2.5 py-2 bg-[#E8C2A5] rounded-full text-[#895025] font-medium">
+                  {slide.buttonText1}
+                </button>
+                <button className="group flex items-center gap-2 px-6 py-2.5 font-medium">
+                  {slide.buttonText2}
+                  <img className="group-hover:translate-x-1 transition" src={assets.arrow_icon} alt="arrow_icon" />
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center flex-1 justify-center ">
+              <img
+                className="md:w-72 w-48"
+                src={slide.imgSrc}
+                alt={`Slide ${index + 1}`}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex items-center justify-center gap-2 mt-8">
+        {sliderData.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => handleSlideChange(index)}
+            className={`h-2 w-2 rounded-full cursor-pointer ${
+              currentSlide === index ? "bg-[#CD7736]" : "bg-gray-500/30"
+            }`}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HeaderSlider;
