3b68dbdbaab705f88c26258e523980d03493d06e
Bethmi Jayamila <bethmij@gmail.com>
2025-03-19 00:17:15 +0530

    frontend -  add assets

---
+++ frontend/src/assets/assets.ts
@@ -0,0 +1,41 @@
+import logo from "./logo.png";
+import search_icon from "./search_icon.svg";
+import user_icon from "./user_icon.svg";
+import cart_icon from "./cart_icon.svg";
+import add_icon from "./add_icon.svg";
+import instagram_icon from "./instagram_icon.svg";
+import facebook_icon from "./facebook_icon.svg";
+import twitter_icon from "./twitter_icon.svg";
+import box_icon from "./box_icon.svg";
+import menu_icon from "./menu_icon.svg";
+import arrow_icon from "./arrow_icon.svg";
+import increase_arrow from "./increase_arrow.svg";
+import decrease_arrow from "./decrease_arrow.svg";
+import arrow_right_icon_colored from "./arrow_right_icon_colored.svg";
+import arrow_icon_white from "./arrow_icon_white.svg";
+import heart_icon from "./heart_icon.svg";
+import star_icon from "./star_icon.svg";
+import redirect_icon from "./redirect_icon.svg";
+import star_dull_icon from "./star_dull_icon.svg";
+
+export const assets = {
+  logo,
+  search_icon,
+  user_icon,
+  cart_icon,
+  add_icon,
+  instagram_icon,
+  facebook_icon,
+  twitter_icon,
+  box_icon,
+  menu_icon,
+  arrow_icon,
+  increase_arrow,
+  decrease_arrow,
+  arrow_right_icon_colored,
+  arrow_icon_white,
+  heart_icon,
+  star_icon,
+  redirect_icon,
+  star_dull_icon,
+};
